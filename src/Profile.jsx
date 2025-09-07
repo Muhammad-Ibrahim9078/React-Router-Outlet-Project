476d5126a8b6f81b2d6916798b131c023b3e32f0
Muhammad Ibrahim <m.iibrahim9078@gmail.com>
2025-09-08 01:31:26 +0500

    some basic changes

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,11 +3,12 @@ import { Link, Outlet } from 'react-router-dom'
 
 function Profile() {
   return (
-    <div className="flex h-screen w-full bg-gray-900 text-white">
+    <div className="flex flex-col sm:flex-row h-screen w-full bg-gray-900 text-white">
+      
       {/* Sidebar */}
-      <div className="sm:w-[15%] lg:w-[18%] bg-gray-800 p-6">
+      <div className="w-full sm:w-[15%] lg:w-[18%] bg-gray-800 p-6">
         <h2 className="text-xl font-bold mb-6">Profile Menu</h2>
-        <ul className="space-y-4">
+        <ul className="space-y-4 sm:">
           <li>
             <Link to="/profile/setting" className="block py-2 px-3 rounded-lg hover:bg-gray-700 transition">Setting</Link>
           </li>
@@ -15,17 +16,14 @@ function Profile() {
             <Link to="/profile/account" className="block py-2 px-3 rounded-lg hover:bg-gray-700 transition">Account</Link>
           </li>
           <li>
-            <Link to="/profile/security" className="block py-2 px-3 rounded-lg hover:bg-gray-700 transition"> Security </Link>
+            <Link to="/profile/security" className="block py-2 px-3 rounded-lg hover:bg-gray-700 transition">Security</Link>
           </li>
         </ul>
       </div>
 
       {/* Content Area */}
       <div className="flex-1 p-6 overflow-y-auto">
-
-
         <Outlet />
-        
       </div>
     </div>
   )
